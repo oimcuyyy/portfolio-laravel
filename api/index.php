550c45b6531a46a3e8f4a0e588a505553573d7a2
oimcuyyy <gegerochim567@gmail.com>
2026-08-02 22:30:16 +0700

    Fix api/index.php runtime crash for Vercel

--- api/index.php
+++ api/index.php
@@ -1,13 +1,13 @@
 <?php
 
-// 1. Buat folder temporary di /tmp untuk Vercel
+// 1. Pastikan folder penyimpanan sementara (/tmp) dibuat sebelum Laravel running
 $storageDirs = [
     '/tmp/storage/app/public',
     '/tmp/storage/framework/views',
     '/tmp/storage/framework/cache/data',
     '/tmp/storage/framework/sessions',
     '/tmp/storage/logs',
-    '/tmp/bootstrap/cache'
+    '/tmp/bootstrap/cache',
 ];
 
 foreach ($storageDirs as $dir) {
@@ -16,9 +16,26 @@ foreach ($storageDirs as $dir) {
     }
 }
 
-// 2. Set variabel environment storage ke /tmp
-putenv('APP_STORAGE_PATH=/tmp/storage');
-putenv('VIEW_COMPILED_PATH=/tmp/storage/framework/views');
+// 2. Set environment path storage & bootstrap cache ke /tmp
+$_ENV['APP_STORAGE_PATH'] = '/tmp/storage';
+$_ENV['APP_SERVICES_CACHE'] = '/tmp/bootstrap/cache/services.php';
+$_ENV['APP_PACKAGES_CACHE'] = '/tmp/bootstrap/cache/packages.php';
+$_ENV['APP_CONFIG_CACHE'] = '/tmp/bootstrap/cache/config.php';
+$_ENV['APP_ROUTES_CACHE'] = '/tmp/bootstrap/cache/routes.php';
 
-// 3. Forward request ke index.php bawaan Laravel
-require __DIR__ . '/../public/index.php';
+// 3. Load Autoloader bawaan Composer
+require __DIR__ . '/../vendor/autoload.php';
+
+// 4. Bootstrapping Laravel Application
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
+// 5. Handle Request
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+);
+
+$response->send();
+
+$kernel->terminate($request, $response);
