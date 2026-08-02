76ebe9f8deb9e38a7bc43624a461334b1777f0ef
oimcuyyy <gegerochim567@gmail.com>
2026-08-02 21:20:35 +0700

    Clean api/index.php and set temp dirs

--- api/index.php
+++ api/index.php
@@ -1,25 +1,20 @@
 <?php
 
-// 1. Buat folder temporary di Vercel
-$storageDirs = [
+// 1. Buat folder temporary wajib Vercel Serverless
+$dirs = [
     '/tmp/storage/app/public',
     '/tmp/storage/framework/views',
     '/tmp/storage/framework/cache/data',
     '/tmp/storage/framework/sessions',
     '/tmp/storage/logs',
-    '/tmp/bootstrap/cache'
+    '/tmp/bootstrap/cache',
 ];
 
-foreach ($storageDirs as $dir) {
+foreach ($dirs as $dir) {
     if (!is_dir($dir)) {
         mkdir($dir, 0755, true);
     }
 }
 
-// 2. Override variabel path Laravel ke /tmp
-$_ENV['APP_STORAGE_PATH'] = '/tmp/storage';
-$_ENV['VIEW_COMPILED_PATH'] = '/tmp/storage/framework/views';
-putenv('VIEW_COMPILED_PATH=/tmp/storage/framework/views');
-
-// 3. Panggil aplikasi Laravel
+// 2. Forward ke index public Laravel
 require __DIR__ . '/../public/index.php';
